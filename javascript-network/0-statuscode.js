63ebeb914d797b845f9014cce90f30eb58a7612c
SamiyaTol <148984827+SamiyaTol@users.noreply.github.com>
2024-02-04 22:48:43 +0100

    Update 0-statuscode.js

--- javascript-network/0-statuscode.js
+++ javascript-network/0-statuscode.js
@@ -1,13 +1,9 @@
-#!/usr/bin/python3
-# Lists all states from the database hbtn_0e_0_usa.
-# Usage: ./0-select_states.py <mysql username> \
-#                             <mysql password> \
-#                             <database name>
-import sys
-import MySQLdb
+#!/usr/bin/node
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM `states`")
-    [print(state) for state in c.fetchall()]
+const request = require('request');
+
+const url = process.argv[2];
+
+request.get(url).on('response', function (response) {
+  console.log(`code: ${response.statusCode}`);
+});
