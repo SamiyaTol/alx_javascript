846958c04d3db387762fea5829f6ae011d125475
SamiyaTol <148984827+SamiyaTol@users.noreply.github.com>
2024-02-04 22:49:47 +0100

    Update 2-starwars_count.js

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -1,29 +1,18 @@
-#!/usr/bin/python3
-# Salama-Jamal (2-my_filter_states.py)
-"""
-return matching states parameters given to script
-"""
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
 
-import MySQLdb
-from sys import argv
-
-if __name__ == "__main__":
-
-    # connect to database
-    db = MySQLdb.connect(host="localhost",
-                         port=3306,
-                         user=argv[1],
-                         passwd=argv[2],
-                         db=argv[3])
-
-    # create cursor to exec queries using SQL; match arg given
-    cursor = db.cursor()
-    sql_cmd = """SELECT *
-                 FROM states
-                 WHERE name LIKE '{:s}' ORDER BY id ASC""".format(argv[4])
-    cursor.execute(sql_cmd)
-    for row in cursor.fetchall():
-        if row[1] == argv[4]:
-            print(row)
-    cursor.close()
-    db.close()
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  }
+  let count = 0;
+  for (const result of JSON.parse(body).results) {
+    for (const wedgeUrl of result.characters) {
+      if (wedgeUrl.includes(18)) {
+        count++;
+      }
+    }
+  }
+  console.log(count);
+});
